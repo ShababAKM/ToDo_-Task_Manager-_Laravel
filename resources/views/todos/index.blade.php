bb7f440ab0d515a74b4067233e4d4a2d28e6200b
A K M Shabab <shababakm.academy@gmail.com>
2020-05-31 12:59:42 +0600

    bootstrape,jquery,font-awesome added

--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -3,6 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('font-awesome/css/font-awesome.min.css') }}" rel="stylesheet">
     <title>Document</title>
 </head>
 <body>
@@ -10,5 +12,7 @@
     @foreach($todos as $todo)
     <li>{{$todo->name}}</li>
     @endforeach
+    <script src="{{ asset('js/jquery.min.js') }}"></script>
+    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
 </body>
 </html>
